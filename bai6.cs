f1b4d5b5ddcfec07df5b58f009b94486f60d8734
datpham1705 <quocdatpham128@gmail.com>
2026-03-20 21:22:10 +0700

    addcode

---
+++ bai6.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+class bai6
+{
+    static void GT()
+    {
+        Console.WriteLine("Pham Quoc Dat");
+        Console.WriteLine("2415053122205");
+        Console.WriteLine("----------------------------");
+        Console.WriteLine("\n\n");
+    }
+    static void Main(string[] args)
+    {
+        GT();
+        List<int> numbers = new List<int> { 6, 2, 3, 8, 5, 4, 1, 7 };
+        Console.WriteLine("Danh sach so nguyen :");
+        foreach (var p in numbers)
+        {
+            Console.Write($"{p} ");
+        }
+        int dem = numbers.Count(x => x%2==0);
+        Console.WriteLine($"\nSo luong so chan : {dem}");
+    }
+}
