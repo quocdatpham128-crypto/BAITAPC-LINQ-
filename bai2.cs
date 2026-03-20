21e2533b558d2400126014df639fc77c0a05e460
datpham1705 <quocdatpham128@gmail.com>
2026-03-20 21:18:44 +0700

    addcode

---
+++ bai2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+class bai2
+{
+    static void GT()
+    {
+        Console.WriteLine("Pham Quoc Dat");
+        Console.WriteLine("2415053122205");
+        Console.WriteLine("----------------------------");
+        Console.WriteLine("\n\n");
+    }
+    static void Main(string[] args)
+    {
+        GT();
+        List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8 };
+        Console.WriteLine("Danh sach so nguyen :");
+        foreach (var p in numbers)
+        {
+            Console.Write($"{p} ");
+        }
+        Console.WriteLine("\nCac so lon hon :");
+        var sochan = numbers.Where(p => p >5);
+        foreach (var p in sochan)
+        {
+            Console.Write($"{p} ");
+        }
+    }
+}
