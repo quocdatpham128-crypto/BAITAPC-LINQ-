24529fa2507e09d16ae193691ddbca6e1c8b2f8b
datpham1705 <quocdatpham128@gmail.com>
2026-03-20 21:26:48 +0700

    addcode

---
+++ bai16.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+class bai16
+{
+    static void GT()
+    {
+        Console.WriteLine("Pham Quoc Dat");
+        Console.WriteLine("2415053122205");
+        Console.WriteLine("----------------------------");
+        Console.WriteLine("\n\n");
+    }
+    static void Main(string[] args)
+    {
+        GT();
+        List<int> ds = new List<int> { 1, 2, 2, 3, 4, 4, 5 };
+        int tong = ds.Sum();
+        Console.WriteLine($"Tong: {tong} ");
+    }
+}
+
