9b5b0c9c09ddedbd6e0aafe8849c194855e85af1
datpham1705 <quocdatpham128@gmail.com>
2026-03-20 21:25:00 +0700

    addcode

---
+++ bai12.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+class Student
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public double Score { get; set; }
+}
+class bai12
+{
+    static void GT()
+    {
+        Console.WriteLine("Pham Quoc Dat");
+        Console.WriteLine("2415053122205");
+        Console.WriteLine("----------------------------");
+        Console.WriteLine("\n\n");
+    }
+    static void Main(string[] args)
+    {
+        GT();
+        List<Student> students = new List<Student>()
+        {
+            new Student { Id = 1, Name = "An", Score = 8 },
+            new Student { Id = 2, Name = "Binh", Score = 6 },
+            new Student { Id = 3, Name = "Chi", Score = 9 },
+            new Student { Id = 4, Name = "Dung", Score = 7 }
+        };
+        int dem = students.Count(x => x.Score >= 7);
+        Console.WriteLine($"So luong sinh vien co diem >= 7: {dem}");
+    }
+}
