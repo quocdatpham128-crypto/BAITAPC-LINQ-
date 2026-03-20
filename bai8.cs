024d9df05dd413ea6c6cb25bdbc23382f0c36f23
datpham1705 <quocdatpham128@gmail.com>
2026-03-20 21:23:05 +0700

    addcode

---
+++ bai8.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+class Student 
+  { 
+    public int Id { get; set; } 
+    public string Name { get; set; }
+    public double Score { get; set; } 
+}
+class bai8
+{
+    static void GT()
+    {
+        Console.WriteLine("Pham Quoc Dat");
+        Console.WriteLine("2415053122205");
+        Console.WriteLine("----------------------------");
+        Console.WriteLine("\n\n");
+    }
+    static void Main(string[] args)
+    {
+        GT();
+        List<Student> students = new List<Student>() 
+        { 
+            new Student { Id = 1, Name = "An", Score = 8 }, 
+            new Student { Id = 2, Name = "Binh", Score = 6 }, 
+            new Student { Id = 3, Name = "Chi", Score = 9 }, 
+            new Student { Id = 4, Name = "Dung", Score = 7 } 
+        };
+        var stg = students.Where(x => x.Score >= 8);
+        Console.WriteLine("Danh sach sinh vien gioi: ");
+        foreach (var s in stg)
+        {
+            Console.WriteLine($"ID: {s.Id} , Name: {s.Name} , Score: {s.Score}");
+        }
+    }
+}
